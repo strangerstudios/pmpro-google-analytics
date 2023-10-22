b03155e87fb64e868df20ea270f3b6fa7326dfa6
Kim Coleman <kim@strangerstudios.com>
2023-10-22 19:14:28 -0400

    Docblocks, new filter for pmproga4_track_view_item_event, user ID tracking, user properties new tracking.

--- includes/functions.php
+++ includes/functions.php
@@ -17,25 +17,52 @@ function pmproga4_load_script() {
         )
     ) );
 
-    // Filter to stop tracking for whatever reason needed. (user ID, post, etc or custom roles etc)
+    /**
+     * Determines whether to halt tracking based on specific conditions.
+     *
+     * This filter provides an opportunity for developers to stop tracking for specific 
+     * scenarios, such as based on user ID, post, custom roles, etc. If the filter returns
+     * `true`, tracking will be halted.
+     *
+     * @since 0.1
+     *
+     * @param bool $stop_tracking Default value is `false`. If set to `true` by any filter, tracking will be halted.
+     *
+     * @return void
+     */
     if ( apply_filters( 'pmproga4_dont_track', false ) ) {
         return;
     }
 
-    // No tracking ID found, lets bail.
+    // No tracking ID found, let's bail.
     if ( empty( $tracking_id ) ) {
         return;
     }
 
-    // Don't track admins if the option is set, added in a filter to allow further customizations and return true/false beyond this.
+    // Don't track admins if the option is set.
     if ( ! empty( $dont_track_admins ) && current_user_can( 'manage_options' ) ) {
         return;
     }
 
-
+    /**
+     * Filters the attributes applied to the Google Analytics script tag.
+     *
+     * Allows developers to customize or add specific attributes to the Google Analytics script tag
+     * for enhanced control or additional features.
+     *
+     * @since 0.1
+     *
+     * @param string $script_atts Default value is an empty string. Contains attributes for the GA script tag.
+     *
+     * @return string Modified attributes for the GA script tag.
+     */
     $script_atts = apply_filters( 'pmproga4_script_atts', '' );
 
+    // Set the custom dimensions array from the helper function.
     $custom_dimensions = pmproga4_custom_dimensions();
+
+    // Set the user properties array from the helper function.
+    $user_properties = pmproga4_user_properties();
     ?>
     <!-- Paid Memberships Pro - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr( $tracking_id ); ?>"></script>
@@ -47,17 +74,27 @@ function pmproga4_load_script() {
             '<?php echo esc_attr( $tracking_id ); ?>', 
             {
                 'currency': '<?php echo get_option( "pmpro_currency" ); ?>',
+                <?php if ( is_user_logged_in() ) { ?>
+                'user_id': '<? echo get_current_user_id(); ?>',
+                <?php } ?>
                 <?php if ( ! empty( $custom_dimensions ) ) { 
                     foreach ( $custom_dimensions as $key => $value ) { ?>
                 '<?php echo esc_attr( $key ); ?>': '<?php echo esc_attr( $value ); ?>',
                 <?php }
-                 } ?>
+                } ?>
+                <?php if ( ! empty( $user_properties ) ) { ?>
+                    'user_properties': {
+                    <?php foreach ( $user_properties as $key => $value ) { ?>
+                    '<?php echo esc_attr( $key ); ?>': '<?php echo esc_attr( $value ); ?>',
+                    <?php } ?>
+                    }
+                <?php } ?>
             }
             );
 		</script>
     <?php
 
-    // Load all helper functions which determine whether to load or not and runs the <scripts>
+    // Load all helper functions for ecommerce events which determine whether to load or not and run the <scripts>.
     pmproga4_view_item_event( $track_levels ); // Levels page
     pmproga4_checkout_events(); // Checkout page includes begin_checkout
     pmproga4_purchase_event(); // Confirmation page, confirmed checkout.
@@ -66,32 +103,57 @@ function pmproga4_load_script() {
 add_action( 'wp_head', 'pmproga4_load_script' );
 
 /**
- * Function to build the view_item event. 
- * Runs on the level select page.
+ * Function for view_item event.
+ * Runs when post_content contains a levels shortcode or block.
  */
 function pmproga4_view_item_event( $track_levels = null ) {
-    global $post, $pmpro_level;
-    
-    // Post content doesn't have the shortcode or the block, bail.
-    if ( strpos( $post->post_content, 'pmpro_levels' ) == false && strpos( $post->post_content, 'pmpro_advanced_levels' ) == false ) {
-        if ( ! has_block( 'pmpro/levels-page' ) ) {
-            return;
-        }
+    global $pmpro_pages;
+
+    /**
+     * Determines whether to track the 'view item' event based on specific conditions.
+     *
+     * This filter allows developers to control when the 'view item' event should be 
+     * tracked. By default, it tracks the event when the current page matches the PMPro levels page.
+     *
+     * @since 0.1
+     *
+     * @param bool $track_view_item_event Default value is determined by whether the current page is the PMPro levels page.
+     *
+     * @return bool Modified flag for whether to track the 'view item' event.
+     */
+    $track_view_item_event = apply_filters( 'pmproga4_track_view_item_event', is_page( $pmpro_pages['levels'] ) );
+
+    // Return if we should not track this event.
+    if ( empty( $track_view_item_event ) ) {
+        return;
     }
 
-    // Make sure pmpro_levels has all levels.
+    // Make sure $pmpro_all_levels has all levels.
     if ( ! isset( $pmpro_all_levels ) ) {
         $pmpro_all_levels = pmpro_getAllLevels( false, true );
     }
 
-    // Get our specific levels for the levels page.
-    $our_levels_to_track = array();
+    /**
+     * Filters the membership levels to be tracked for analytics purposes.
+     *
+     * Allows developers to specify or modify which membership levels should be 
+     * tracked by the Google Analytics integration.
+     *
+     * @since 0.1
+     *
+     * @param array $track_levels Default array of membership level IDs to track from plugin settings.
+     *
+     * @return array Modified array of membership level IDs for tracking.
+     */
     $our_levels = apply_filters( 'pmproga4_track_level_ids', $track_levels );
 
-    // Get all available level ID's if no levels are passed to specifically tra
+    // Get all available level IDs if no levels are passed to specifically track.
     if ( empty( $our_levels ) ) {
         $our_levels = wp_list_pluck( $pmpro_all_levels, 'id' );
     }
+
+    // Set up the array of levels to track.
+    $our_levels_to_track = array();
 
     foreach ( $our_levels as $level_id ) {
         foreach ( $pmpro_all_levels as $level ) {
@@ -108,12 +170,11 @@ function pmproga4_view_item_event( $track_levels = null ) {
     // Create a unique event per level viewed.
     foreach ( $our_levels_to_track as $pmpro_level ) {
 
-        // Build an array of Product Data.
+        // Build an array of product data.
         $gtag_config_ecommerce_products = array();
         $gtag_config_ecommerce_products['item_id'] = 'pmpro-' . $pmpro_level->id;
         $gtag_config_ecommerce_products['item_name'] = $pmpro_level->name;
         $gtag_config_ecommerce_products['affiliation'] = get_bloginfo( 'name' );
-        $gtag_config_ecommerce_products['index'] = 0;
         $gtag_config_ecommerce_products['quantity'] = 1;
 
 
@@ -129,9 +190,9 @@ function pmproga4_view_item_event( $track_levels = null ) {
 <script>
     <?php foreach( $gtag_config_events_push as $gtag_config_event_push ) { ?>
         gtag( 'event', 
-            '<?php echo $gtag_config_event_push["event"]; ?>',  // event type.
+            '<?php echo $gtag_config_event_push["event"]; ?>',  // Event type.
             {
-                value: <?php echo $gtag_config_event_push['value']; ?>, // Value (amount/price)
+                value: <?php echo $gtag_config_event_push['value']; ?>, // Value (initial payment)
                 items: [<?php echo json_encode( $gtag_config_event_push['items'] ); ?>] // Product data.
             }
         ); // End of gtag method.
@@ -141,17 +202,18 @@ function pmproga4_view_item_event( $track_levels = null ) {
 } // End of view_item event.
 
 /**
- * Function for add_to_cart event.
+ * Function for add_to_cart and begin_checkout events.
  * Runs on the Paid Memberships Pro checkout page (page load).
  */
 function pmproga4_checkout_events() {
     global $pmpro_level;
+
     // Only run this on the checkout page.
     if ( ! pmpro_is_checkout() ) {
         return;
     }
-		
-    // Build an array of Product Data.
+
+    // Build an array of product data.
     $gtag_config_ecommerce_products = array();
     $gtag_config_ecommerce_products['item_id'] = 'pmpro-' . $pmpro_level->id;
     $gtag_config_ecommerce_products['item_name'] = $pmpro_level->name;
@@ -169,13 +231,13 @@ function pmproga4_checkout_events() {
     <script>
     jQuery(document).ready(function() {
 
-        // Used later to interact with the
+        // Used later to trigger the begin_checkout event.
         var interacted = 0;
         
         gtag( 'event', 
-            '<?php echo $gtag_config_event_push["event"]; ?>',  // event type.
+            '<?php echo $gtag_config_event_push["event"]; ?>',  // Event type.
             {
-                value: <?php echo $gtag_config_event_push['value']; ?>, // Value (amount/price)
+                value: <?php echo $gtag_config_event_push['value']; ?>, // Value (initial payment).
                 items: [<?php echo json_encode( $gtag_config_event_push['items'] ); ?>] // Product data.
             }
         ); // End of gtag method.
@@ -188,27 +250,27 @@ function pmproga4_checkout_events() {
             return;
         }
 
-        // Begin checkout once the key has been pressed once.
+        // Send the begin_checkout event once the key has been pressed once.
         gtag( 'event', 
-            'begin_checkout',  // event type.
+            'begin_checkout',  // Event type.
             {
-                value: <?php echo $gtag_config_event_push['value']; ?>, // Value (amount/price)
+                value: <?php echo $gtag_config_event_push['value']; ?>, // Value (initial payment).
                 items: [<?php echo json_encode( $gtag_config_event_push['items'] ); ?>] // Product data.
             }
             ); // End of gtag method.
         
-            // local storage to confirm the user has interacted and cross referenced in the purchase event
+            // Use local storage to confirm the user has interacted. Cross referenced in the purchase event.
             localStorage.setItem( 'pmproga4_purchased_level', '<?php echo $pmpro_level->id; ?>' );
 
         interacted++;
         });
     });
     </script>
-        <?php
+    <?php
 }
 
 /**
- * Add purchase event for GA.
+ * Function for purchase event.
  * This loads on the confirmation page and only if there's local storage.
  */
 function pmproga4_purchase_event() {
@@ -218,7 +280,6 @@ function pmproga4_purchase_event() {
     if ( ! is_page( $pmpro_pages['confirmation'] ) ) {
         return;
     }
-
 
     // User completed a free checkout. Get their last invoice for the data layer.
     if ( empty( $pmpro_invoice ) && pmpro_isLevelFree( $current_user->membership_level ) ) {
@@ -278,27 +339,36 @@ function pmproga4_purchase_event() {
                 items: [ <?php echo json_encode( $gtag_config_ecommerce_products ); ?> ]
             });
             });
-
         </script>
         <?php
-
     }
 }
 
 /**
  * Custom Dimensions that _we_ need.
- * @returns array $custom_dimensions The custom dimensions we want as a key=>value pair.
+ *
+ * @return array $gtag_config_custom_dimensions Custom dimensions as a key=>value pair.
  */
 function pmproga4_custom_dimensions() {
-    // Set up the Custom Dimension data.
+    /**
+     * Sets up the Custom Dimension data for Google Analytics.
+     *
+     * This function allows for the customization of default custom dimensions
+     * by applying the 'pmproga4_default_custom_dimension' filter.
+     *
+     * @since 0.1
+     *
+     * @param array $gtag_config_custom_dimensions An array of custom dimensions as key=>value pairs.
+     *
+     * @return array Filtered custom dimensions.
+     */
 	$gtag_config_custom_dimensions = apply_filters( 'pmproga4_default_custom_dimension', array( 
         'post_type' => '', 
         'author' => '', 
-        'category' => '', 
-        'membership_level' => ''
+        'category' => ''
      ) );
 
-    // Track 'post_type' dimensions.
+    // Track 'post_type' dimension.
     if ( isset( $gtag_config_custom_dimensions['post_type'] ) ) {
         $post_type = '';
         if ( is_singular() ) {
@@ -311,21 +381,15 @@ function pmproga4_custom_dimensions() {
         }
     }
 
-    // Track 'author' data  
+    // Track 'author' dimension.
     if ( isset( $gtag_config_custom_dimensions['author'] ) ) {
         $author = '';
         if ( is_singular() ) {
             if ( have_posts() ) {
                 while ( have_posts() ) {
                     the_post();
+                    $author = get_the_author_meta( 'display_name' );
                 }
-            }
-            $firstname = get_the_author_meta( 'user_firstname' );
-            $lastname  = get_the_author_meta( 'user_lastname' );
-            if ( ! empty( $firstname ) || ! empty( $lastname ) ) {
-                    $author = trim( $firstname . ' ' . $lastname );
-            } else {
-                $author = 'user-' . get_the_author_meta( 'ID' );
             }
         }
         if ( ! empty( $author ) ) {
@@ -335,7 +399,7 @@ function pmproga4_custom_dimensions() {
         }
     }
 
-    // Track post category if applicable.
+    // Track post category, if applicable.
     if ( isset( $gtag_config_custom_dimensions['category'] ) ) {
         $category = '';
         if ( is_singular( 'post' ) ) {
@@ -355,12 +419,46 @@ function pmproga4_custom_dimensions() {
         }
     }
 
+    /**
+     * Filters the custom dimensions to allow developers to add or remove custom dimensions.
+     *
+     * @since 0.1
+     *
+     * @param array $gtag_config_custom_dimensions Custom dimensions as a key=>value pair.
+     * @return array Filtered custom dimensions as a key=>value pair.
+     */
+    return apply_filters( 'pmproga4_custom_dimensions', $gtag_config_custom_dimensions );
+}
+
+/**
+ * User Properties to track.
+ *
+ * @return array $gtag_config_user_properties User properties as a key=>value pair.
+ */
+
+function pmproga4_user_properties() {
+    /**
+     * Sets up the User Properties data for Google Analytics.
+     *
+     * This function allows for the customization of default user properties
+     * by applying the 'pmproga4_default_user_properties' filter.
+     *
+     * @since 0.1
+     *
+     * @param array $gtag_config_user_properties An array of user properties as key=>value pairs.
+     *
+     * @return array Filtered user properties.
+     */
+	$gtag_config_user_properties = apply_filters( 'pmproga4_default_user_properties', array(
+        'membership_level' => ''
+     ) );
+
     // Track members membership level.
-    if ( isset( $gtag_config_custom_dimensions['membership_level'] ) ) {
+    if ( isset( $gtag_config_user_properties['membership_level'] ) ) {
         $membership_level = '';
         // Get the value to track for the current user.
         if ( is_user_logged_in() && function_exists( 'pmpro_getMembershipLevelForUser' ) ) {
-            // Get the current users's membership level ID. 
+            // Get the current user's membership level ID.
             $current_user_membership_level = pmpro_getMembershipLevelForUser( get_current_user_id() );
             if ( empty( $current_user_membership_level ) ) {
                 // Set the tracked membership level ID to no_level.
@@ -373,14 +471,17 @@ function pmproga4_custom_dimensions() {
             $membership_level = 'no_level';
         }
         if ( ! empty( $membership_level ) ) {
-            $gtag_config_custom_dimensions['membership_level'] = esc_html( $membership_level );
+            $gtag_config_user_properties['membership_level'] = esc_html( $membership_level );
         }
     }
 
-    /**
-     * Filter the custom dimensions we want to track and allow developers to add or remove custom dimensions.
-     * @param array $gtag_config_custom_dimensions The custom dimensions we want as a key=>value pair.
-     * @return array $gtag_config_custom_dimensions The custom dimensions we want as a key=>value pair.
+   /**
+     * Filters the user properties to allow developers to add or remove properties.
+     *
+     * @since 0.1
+     *
+     * @param array $gtag_config_user_properties User properties as a key=>value pair.
+     * @return array Filtered user properties as a key=>value pair.
      */
-    return apply_filters( 'pmproga4_custom_dimensions', $gtag_config_custom_dimensions );
+    return apply_filters( 'pmproga4_user_properties', $gtag_config_user_properties );
 }
